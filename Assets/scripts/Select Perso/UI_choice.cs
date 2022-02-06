cf0fa2976afdcceec56cd39755feed530475f56b
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-02-06 22:01:08 +0100

    added some gadjet to pimp the project

--- Assets/scripts/Select Perso/UI_choice.cs
+++ Assets/scripts/Select Perso/UI_choice.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace scripts
 {
@@ -9,9 +10,32 @@ namespace scripts
         public bool state;
         [SerializeField]
         GameObject Exelcia;
+        [SerializeField]
+        Text txt;
+        [SerializeField]
+        GameObject canvas;
+
+        public string ip_local;
+        public static string GetLocalIPAddress()
+        {
+            var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            throw new System.Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+
         void Start()
         {
             state = true;
+            ip_local = GetLocalIPAddress();
+            txt.text = "ip_local : " + ip_local;
         }
 
         // Update is called once per frame
@@ -20,7 +44,10 @@ namespace scripts
             if (state)
                 state = !GameManager.GetStateStart();
             else
+            {
                 Exelcia.SetActive(false);
+                canvas.SetActive(false);
+            }
         }
     }
 }
