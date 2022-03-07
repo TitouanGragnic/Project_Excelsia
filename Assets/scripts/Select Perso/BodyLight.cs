ce2a3b6515df1dd9fb091d7191f2b2024affcbb8
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-03-07 16:37:13 +0100

    fix bug of restart

--- Assets/scripts/Select Perso/BodyLight.cs
+++ Assets/scripts/Select Perso/BodyLight.cs
@@ -4,68 +4,80 @@ using UnityEngine;
 using Mirror;
 
 
-public class BodyLight : NetworkBehaviour
+namespace scripts
 {
-    [SerializeField]
-    public Light light_up;
-
-    [SyncVar]
-    public bool choised;
-
-    [SerializeField]
-    GameObject P1;
-    [SerializeField]
-    GameObject P2;
-
-    [SerializeField]
-    [SyncVar]
-    public int Pnb;
-
-
-
-
-    void Start()
+    public class BodyLight : NetworkBehaviour
     {
-        light_up.enabled = false;
-        P1.SetActive(false);
-        P2.SetActive(false);
-        Pnb = 0;
+        [SerializeField]
+        public Light light_up;
 
-    }
+        [SyncVar]
+        public bool choised;
 
-    void Update()
-    {
-        light_up.enabled = choised;
-        UpdateGOup();
-    }
+        [SerializeField]
+        GameObject P1;
+        [SerializeField]
+        GameObject P2;
 
-    void UpdateGOup()
-    {
-        if (Pnb == 1)
-            P1.SetActive(true);
-        else
+        [SerializeField]
+        [SyncVar]
+        public int Pnb;
+
+
+
+
+        void Start()
+        {
+            light_up.enabled = false;
             P1.SetActive(false);
-        if (Pnb == 2)
-            P2.SetActive(true);
-        else
             P2.SetActive(false);
-    }
+            Pnb = 0;
 
-    [Command(requiresAuthority = false)]
-    public void Select(int Pnb)
-    {
-        this.Pnb = Pnb;
-        choised = true;
-        light_up.enabled = true;
-    }
+        }
 
-    [Command(requiresAuthority = false)]
-    public void UnSelect()
-    {
-        Pnb = 0;
-        choised = false;
-        light_up.enabled = false;
+        void Update()
+        {
+            light_up.enabled = choised;
+            UpdateGOup();
+            if (GameManager.GetStateSpawn())
+                Restore();
+        }
 
+
+        void Restore()
+        {
+            Pnb = 0;
+            choised = false;
+        }
+
+        void UpdateGOup()
+        {
+            if (Pnb == 1)
+                P1.SetActive(true);
+            else
+                P1.SetActive(false);
+            if (Pnb == 2)
+                P2.SetActive(true);
+            else
+                P2.SetActive(false);
+        }
+
+        [Command(requiresAuthority = false)]
+        public void Select(int Pnb)
+        {
+            this.Pnb = Pnb;
+            choised = true;
+            light_up.enabled = true;
+        }
+
+        [Command(requiresAuthority = false)]
+        public void UnSelect()
+        {
+            Pnb = 0;
+            choised = false;
+            light_up.enabled = false;
+
+        }
     }
 }
 
