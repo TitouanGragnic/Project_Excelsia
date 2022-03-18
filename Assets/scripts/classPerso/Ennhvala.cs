81a687c1d7457d6f9c8cd4058ed3801bfea591e9
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-03-19 00:25:19 +0100

    added activ Tamo

--- Assets/scripts/classPerso/Ennhvala.cs
+++ Assets/scripts/classPerso/Ennhvala.cs
@@ -12,6 +12,12 @@ namespace scripts
         [SerializeField]
         Camera cam;
 
+        public int actifCooldown;
+        private int actifCooldownMax = 7000;
+        public bool actifState;
+
+
+
         public float malus = 0f;
         public float bonus = 1f;
 
@@ -24,24 +30,48 @@ namespace scripts
             
             armor = 0.1f;
             atk = 25f;
+
+
+            //actif
+            actifCooldown = actifCooldownMax;
+            actifState = false;
         }
 
         private void Update()
         {
-
+            CoolDown();
             //this.health -= malus;
             //this.atk = atk * (maxHealth / health) * bonus;
         }
-
+        private void CoolDown()
+        {
+            if (actifCooldown <= 0 && actifState)
+                EndActifCoolDown();
+            if (actifCooldown > 0)
+                actifCooldown -= 1;
+        }
+        void EndActifCoolDown()
+        {
+            actifState = false;
+            actifCooldown = 0;
+        }
         public new void Actif()
         {
-            SpawnKnife();
+            if (actifCooldown == 0) 
+            {
+                SpawnKnife();
+                actifState = true;
+                actifCooldown = actifCooldownMax;
+            }
+            
         }
 
         private void SpawnKnife()
         {
             GameObject kn = Instantiate(knife);
             kn.transform.position = arm.transform.position + cam.transform.forward.normalized*2;
+            kn.transform.forward = cam.transform.forward;
+            kn.GetComponent<Knife>().rotate = cam.transform.forward;
             Rigidbody rb = kn.GetComponent<Rigidbody>();
             rb.AddForce(cam.transform.forward.normalized * 5, ForceMode.Impulse);
         }
---
+++ Assets/scripts/classPerso/Mine.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace scripts
+{
+    public class Mine : NetworkBehaviour
+    {
+        // Start is called before the first frame update
+        public Vector3 rotate;
+        bool touche = false;
+        bool stick = false;
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.layer == 9 && !touche)// layer client
+            {
+                col.gameObject.GetComponent<Perso>().TakeDamage(500, "normal");
+                touche = true;
+
+                NetworkServer.Destroy(this.gameObject);
+            }
+            if ((col.gameObject.layer == 7 || col.gameObject.layer == 11) && !stick)// layer wall or ground
+            {
+                stick = true;
+                Rigidbody rb = GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.FreezePosition;
+            }
+        }
+
+        void Update()
+        {
+            if(!stick)
+                transform.Rotate(rotate * 5);
+        }
+    }
+}
