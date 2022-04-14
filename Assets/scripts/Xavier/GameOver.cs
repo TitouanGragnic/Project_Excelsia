465885c82b2e11cb1ece7ef834beeec1d09eb964
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-04-14 22:33:44 +0200

    fix xavier commit

--- Assets/scripts/Xavier/GameOver.cs
+++ Assets/scripts/Xavier/GameOver.cs
@@ -13,14 +13,18 @@ namespace scripts
         GameObject PlayerPrefab;
 
 
+
         private void Start()
         {
             Cursor.lockState = CursorLockMode.None;
             GameManager.CmdAtributPnb(name);
+
+
         }
 
         void Update()
         {
+            Rotatearound();
             if (Input.GetKeyDown(KeyCode.E))
                 NetworkManager.singleton.StopHost(); 
 
@@ -35,10 +39,18 @@ namespace scripts
             string netId = GetComponent<NetworkIdentity>().netId.ToString();
             GameManager.RegisterChoice(netId, SnewPlayer.GetComponent<choice>());
 
-            SnewPlayer.transform.position = new Vector3(989.7f, 54f, -50.5f);// la camera dans le menu choice de titouan
+            SnewPlayer.transform.position = new Vector3(995.27f, 1.84f, -13.88f);// la camera dans le menu choice de titouan
             GameObject SoldPlayer = connectionToClient.identity.gameObject;
             NetworkServer.ReplacePlayerForConnection(connectionToClient, SnewPlayer, true);
             NetworkServer.Destroy(SoldPlayer);
+        }
+
+        [Command]
+        [Client]
+        public void Rotatearound()
+        {
+            transform.LookAt(new Vector3(0f, 0f, 0f));
+            transform.RotateAround(new Vector3(0,0,0), Vector3.up, 20 * Time.deltaTime); // on vas faire tourner les nouveaux prefab autour du nouveau game object qui est un prefab en (0,0,0)
         }
     }
 }
--- Assets/scripts/Xavier/endMenu.cs
+++ Assets/scripts/Xavier/endMenu.cs
@@ -10,8 +10,6 @@ namespace scripts
         [SerializeField]
         GameObject PlayerPrefab;
 
-        [SerializeField]
-        GameObject target;
 
         public static bool state = false;
         
@@ -21,31 +19,20 @@ namespace scripts
                 Cmd_ReplacePlayer();
 
         }
-        void Start() 
-        {
-            Rotatearound();
-        }
 
 
         [Command][Client]
         public void Cmd_ReplacePlayer()
         {
             GameObject newPlayer = Instantiate(PlayerPrefab);
-            newPlayer.transform.position = new Vector3(20f, 20f, 20f);
+            newPlayer.transform.position = new Vector3(300f, 150f, 300f);
+            newPlayer.transform.rotation = new Quaternion(20f, 0f, 0f,0f);
             GameObject oldPlayer = connectionToClient.identity.gameObject;
             NetworkServer.ReplacePlayerForConnection(connectionToClient, newPlayer, true);
             NetworkServer.Destroy(oldPlayer);
         }
 
-        [Command][Client]
-        public void Rotatearound()
-        {
-            GameObject Player = connectionToClient.identity.gameObject; // on recupere notre perso 
-            target = Instantiate(PlayerPrefab); // une target est défini
-            target.transform.position = new Vector3(30f, 30f, 30f);  // ont la place en (0,0,0)
-            Player.transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime); // on vas faire tourner les nouveaux prefab autour du nouveau game object qui est un prefab en (0,0,0)
-            NetworkServer.Destroy(target);
-        }
+        
 
     }
 }
