7e9dbd3f0d5d137bfc865b0ea6b40691d97f2826
xavier.ghostine-abi-nassif <xavier.ghostine-abi-nassif@epita.fr>
2022-04-14 20:36:36 +0100

    xavier's end menu rotate scripts and end's UI

--- Assets/scripts/Xavier/endMenu.cs
+++ Assets/scripts/Xavier/endMenu.cs
@@ -10,11 +10,22 @@ namespace scripts
         [SerializeField]
         GameObject PlayerPrefab;
 
+        [SerializeField]
+        GameObject target;
+
+        public static bool state = false;
+        
         void Update()
         {
             if (GameManager.GetWinState(name) || GameManager.GetLooseState(name))
                 Cmd_ReplacePlayer();
+
         }
+        void Start() 
+        {
+            Rotatearound();
+        }
+
 
         [Command][Client]
         public void Cmd_ReplacePlayer()
@@ -26,7 +37,15 @@ namespace scripts
             NetworkServer.Destroy(oldPlayer);
         }
 
-
+        [Command][Client]
+        public void Rotatearound()
+        {
+            GameObject Player = connectionToClient.identity.gameObject; // on recupere notre perso 
+            target = Instantiate(PlayerPrefab); // une target est défini
+            target.transform.position = new Vector3(30f, 30f, 30f);  // ont la place en (0,0,0)
+            Player.transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime); // on vas faire tourner les nouveaux prefab autour du nouveau game object qui est un prefab en (0,0,0)
+            NetworkServer.Destroy(target);
+        }
 
     }
 }
