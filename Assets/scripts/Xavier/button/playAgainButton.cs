1b833d73541a21a0c310d5545a118cb0f2afdca5
Titouan <titouan.guesdon@epita.fr>
2022-05-27 16:23:29 +0200

    choice tamo

--- Assets/scripts/Xavier/button/playAgainButton.cs
+++ Assets/scripts/Xavier/button/playAgainButton.cs
@@ -10,20 +10,42 @@ namespace scripts
     {
         [SerializeField]
         GameObject PlayerPrefab;
+        [SerializeField]
+        NetworkManager network;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Cmd_ReplacePlayer();
+            }
+        }
 
         [Command(requiresAuthority = false)]
-        //[Client]
+        [Client]
         public void Cmd_ReplacePlayer()
         {
+            if (isServer)
+            {
+                network.StopHost();
+                network.StartClient();
+                network.StartHost();
+            }
+            else
+            {
+                string coucou = network.networkAddress;
+                network.StopClient();
+                network.networkAddress = coucou;
+                network.StartClient();
+            }
 
-            GameObject SnewPlayer = Instantiate(PlayerPrefab);
+            /*GameObject SnewPlayer = Instantiate(PlayerPrefab);
             string netId = GetComponent<NetworkIdentity>().netId.ToString();
             GameManager.RegisterChoice(netId, SnewPlayer.GetComponent<choice>());
 
             SnewPlayer.transform.position = new Vector3(995.27f, 1.84f, -13.88f);// la camera dans le menu choice de titouan
             GameObject SoldPlayer = connectionToClient.identity.gameObject;
             NetworkServer.ReplacePlayerForConnection(connectionToClient, SnewPlayer, true);
-            NetworkServer.Destroy(SoldPlayer);
+            NetworkServer.Destroy(SoldPlayer);*/
         }
     }
 }
