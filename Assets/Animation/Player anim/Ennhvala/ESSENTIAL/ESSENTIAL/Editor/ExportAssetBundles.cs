226eaa4737d2242f5a33a70af3c832f4bce24077
Titouan <titouan.guesdon@epita.fr>
2022-05-29 20:41:45 +0200

    fix build and run

---
+++ Assets/Animation/Player anim/Ennhvala/ESSENTIAL/ESSENTIAL/Editor/ExportAssetBundles.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildAssetBundlesExample : MonoBehaviour
+{
+    [MenuItem("Bundle/Build AssetBundles")]
+    static void BuildABs()
+    {
+		//puts the bundles in the project folder
+        BuildPipeline.BuildAssetBundles(Application.dataPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows); 
+    }
+}
