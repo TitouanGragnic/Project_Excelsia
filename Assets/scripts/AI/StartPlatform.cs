6b366e44a57fab78462624a1f6857a027fe9ac80
Titouan <titouan.guesdon@epita.fr>
2022-06-01 18:03:17 +0200

    sound

--- Assets/scripts/AI/StartPlatform.cs
+++ Assets/scripts/AI/StartPlatform.cs
@@ -9,6 +9,13 @@ namespace scripts
     {
         [SerializeField]
         public int dir;
+        [SerializeField]
+        public AudioSource sound;
+
+        [SerializeField]
+        public AudioClip[] lect;
+
+        public bool begin = true;
         Vector3 start;
         public int move = 0;
 
@@ -25,12 +32,25 @@ namespace scripts
                 Translate();
             if (transform.position != start && GameManager.players.Count == 0)
                 Restart();
+            if(move == 4000)
+            {
+                sound.clip = lect[1];
+                sound.Play();
+                sound.loop = false;
+            }
 
             CLientFixPos(transform.position, transform.rotation);
         }
 
         public void Translate()
         {
+            if (begin)
+            {
+                sound.clip = lect[0];
+                sound.Play();
+                sound.loop = true;
+                begin = false;
+            }
             gameObject.transform.Translate(Vector3.forward * dir*0.001f);
             move += 1;
         }
