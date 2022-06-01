d9454a23ee83f98161ab1702c7143f7841b7c2f4
Titouan <titouan.guesdon@epita.fr>
2022-06-02 00:21:32 +0200

    banniere

--- Assets/banniere.cs
+++ Assets/banniere.cs
@@ -7,8 +7,9 @@ namespace scripts
 {
     public class banniere : MonoBehaviour
     {
-        [SerializeField] Texture[] diff;
+        [SerializeField] Material[] diff;
         [SerializeField] GameObject screen;
+        [SerializeField] GameObject screen1;
 
         public bool yes = true;
 
@@ -26,46 +27,44 @@ namespace scripts
                 {
                     if (player.Value.personnage == "Ennhvala" && !yes)
                     {
-                        screen.GetComponent<MeshRenderer>().materials[3].SetTexture("ennhvala", diff[0]);
+                        screen.GetComponent<MeshRenderer>().material = diff[0];
                     }
                     if (player.Value.personnage == "Gally" && !yes)
                     {
-                        screen.GetComponent<MeshRenderer>().materials[3].SetTexture("gally", diff[1]);
+                        screen.GetComponent<MeshRenderer>().material = diff[1];
                     }
                     if (player.Value.personnage == "Idriss" && !yes)
                     {
-                        screen.GetComponent<MeshRenderer>().materials[3].SetTexture("idriss", diff[2]);
+                        screen.GetComponent<MeshRenderer>().material = diff[2];
                     }
-
                     if (player.Value.personnage == "Tamo" && !yes)
                     {
-                        screen.GetComponent<MeshRenderer>().materials[3].SetTexture("tamo", diff[3]);
+                        screen.GetComponent<MeshRenderer>().material = diff[3];
                     }
                     if (player.Value.personnage == "Ennhvala" && yes)
                     {
-                        screen.GetComponent<MeshRenderer>().materials[0].SetTexture("ennhvala", diff[0]);
+                        screen1.GetComponent<MeshRenderer>().material = diff[0];
                         yes = false;
                     }
                     if (player.Value.personnage == "Gally" && yes)
                     {
-                        screen.GetComponent<MeshRenderer>().materials[0].SetTexture("gally", diff[1]);
+                        screen1.GetComponent<MeshRenderer>().material = diff[1];
                         yes = false;
                     }
                     if (player.Value.personnage == "Idriss" && yes)
                     {
                         Debug.Log(player.Value.personnage);
-                        screen.GetComponent<MeshRenderer>().materials[0].SetTexture("idriss", diff[2]);
+                        screen1.GetComponent<MeshRenderer>().material = diff[2];
                         Debug.Log(screen.GetComponent<MeshRenderer>().materials[0]);
                         Debug.Log(diff[2]);
                         yes = false;
                     }
                     if (player.Value.personnage == "Tamo" && yes)
                     {
-                        screen.GetComponent<MeshRenderer>().materials[0].SetTexture("tamo", diff[3]);
+                        screen1.GetComponent<MeshRenderer>().material = diff[3];
                         yes = false;
                     }
                 }
-                yes = true;
             }
         }
     }
