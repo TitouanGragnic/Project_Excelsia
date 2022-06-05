7be98e4b3883f230498f4089201cd36451ee80d8
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-06-05 17:01:52 +0200

    try to fix camera rotation

--- Assets/BeginScripts.cs
+++ Assets/BeginScripts.cs
@@ -5,40 +5,25 @@ using UnityEngine;
 public class BeginScripts : MonoBehaviour
 {
 
-    public Behaviour[] beginSetup;
-    public GameObject cam;
-    public GameObject begin;
-    public GameObject canvas;
-    public Animator beginning;
     float time;
+    public GameObject begin;
 
     // Start is called before the first frame update
     void Start()
-    {
-        time = Time.time;
-        cam.SetActive(false);
+    {       
         begin.SetActive(true);
-        canvas.SetActive(false);
-        beginning.Play("move");
-        begin.transform.position = new Vector3(32, 1389, -300);
         time = Time.time;
-        for(int i = 0; i<beginSetup.Length; i++)
-        {
-            beginSetup[i].enabled = false;
-        }
+        begin.transform.position = new Vector3(32, 1389, -300);
     }
 
+    BeginDesactive desScript;
     // Update is called once per frame
     void Update()
     {
         if (Time.time-time > 4)
         {
-            cam.SetActive(true);
-            canvas.SetActive(true);
-            for (int i = 0; i < beginSetup.Length; i++)
-            {
-                beginSetup[i].enabled = true;
-            }
+            if (desScript.enabled)
+                desScript.Desactivate();
             begin.SetActive(false);
         }
     }
