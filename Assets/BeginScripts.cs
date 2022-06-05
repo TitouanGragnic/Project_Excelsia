c15618537f4055dad9abb0e13ee0f07e27d1f007
Titouan <titouan.guesdon@epita.fr>
2022-06-06 00:04:33 +0200

    new begin3

--- Assets/BeginScripts.cs
+++ Assets/BeginScripts.cs
@@ -24,6 +24,10 @@ public class BeginScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.z > 0)
+            begin.transform.position = new Vector3(begin.transform.position.x, begin.transform.position.y, -300);
+        else
+            begin.transform.position = new Vector3(begin.transform.position.x, begin.transform.position.y, 300);
         if (Time.time-time > 4)
         {
             if (desScript.enabled)
