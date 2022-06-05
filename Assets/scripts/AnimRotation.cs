5053836c5e0471dd63d5372f6c37441e214a4ef4
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-06-05 18:30:38 +0200

    try to fix pos

--- Assets/scripts/AnimRotation.cs
+++ Assets/scripts/AnimRotation.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class AnimRotation : MonoBehaviour
+public class AnimRotation : NetworkBehaviour
 {
     [SerializeField] Transform camera;
+    [SerializeField] Transform target;
     public Vector3 pos;
 
     public Vector3 normalPos;
@@ -12,12 +14,33 @@ public class AnimRotation : MonoBehaviour
 
     void Update()
     {
-        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, camera.rotation.eulerAngles.y, transform.rotation.z));
-        transform.localPosition = pos;
+        target.transform.rotation = Quaternion.Euler(new Vector3(target.transform.rotation.x, camera.rotation.eulerAngles.y, target.transform.rotation.z));
+        target.transform.localPosition = pos;
+
+        CmdFixPos(target.transform.position, target.transform.rotation);
     }
+    [Command(requiresAuthority = false)]
     public void Crouch(bool state)
     {
+        RpcCrouch(state);
+    }
+    [ClientRpc]
+    void RpcCrouch(bool state)
+    {
         pos = state ? crouchPos : normalPos;
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdFixPos(Vector3 pos, Quaternion rot)
+    {
+        RpcFixPos(pos, rot);
+    }
+
+    [ClientRpc]
+    void RpcFixPos(Vector3 pos, Quaternion rot)
+    {
+        target.transform.position = pos;
+        target.transform.rotation = rot;
     }
 }
 
