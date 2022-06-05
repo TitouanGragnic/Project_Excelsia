41c9579c6c8e7df4def1261590c35efc7dfeb14f
Titouan <titouan.guesdon@epita.fr>
2022-06-05 23:56:09 +0200

    new begin1

--- Assets/BeginScripts.cs
+++ Assets/BeginScripts.cs
@@ -14,7 +14,7 @@ public class BeginScripts : MonoBehaviour
     {       
         begin.SetActive(true);
         time = Time.time;
-        if(transform.position.z < 0)
+        if(transform.position.z > 0)
             begin.transform.position = new Vector3(32, 1389, -300);
         else
             begin.transform.position = new Vector3(32, 1389, 300);
